cc6c913dc821f125b381da3c83f6116a72fa0151
73ammar <ammar1234963@gmail.com>
2024-03-27 16:39:26 +0530

    first commit

---
+++ account/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('/login/', {'msg': msg})
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form, 'msg': msg})
+
+
+@csrf_exempt
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_admin:
+                login(request, user)
+                return redirect('adminpage')
+            elif user is not None and user.is_customer:
+                login(request, user)
+                return redirect('customer')
+            elif user is not None and user.is_employee:
+                login(request, user)
+                return redirect('employee')
+            else:
+                msg = 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'login.html', {'form': form, 'msg': msg})
+
+
+@csrf_exempt
+def admin(request):
+    return render(request, 'admin.html')
+
+
+@csrf_exempt
+def customer(request):
+    return render(request, 'customer.html')
+
+
+@csrf_exempt
+def employee(request):
+    return render(request, 'employee.html')
